91f40b7d29bd1d38d64c6eb5f022cb42dbf8aec9
Darien Edwards <edwards.darien@outlook.com>
2023-05-26 17:19:37 -0400

    Add gif and made it play faster with JS

---
+++ recipe.js
@@ -0,0 +1,8 @@
+// Basic javascript for my webpage
+
+document.addEventListener("DOMContentLoaded", function() {
+    const video = document.getElementById("myvideo");
+    video.playbackRate = 1.5;
+    console.log(video.playbackRate); // expected output: 1.5
+
+});
